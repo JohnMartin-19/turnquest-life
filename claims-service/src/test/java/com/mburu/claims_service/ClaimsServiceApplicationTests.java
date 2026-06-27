fa58d6725bc96ded317aaac2528884675b082e62
JohnMartin-19 <johnnkonge2020@gmail.com>
2026-06-27 05:34:20 +0300

    Created the overall project structure

---
+++ claims-service/src/test/java/com/mburu/claims_service/ClaimsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mburu.claims_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ receipts-service/src/test/java/com/mburu/receipts_service/ReceiptsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mburu.receipts_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

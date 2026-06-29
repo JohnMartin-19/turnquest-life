c37fcdba94cae2d8a3a56d9255792efdd02356b2
JohnMartin-19 <johnnkonge2020@gmail.com>
2026-06-29 05:59:10 +0300

    added the JpaConfig

---
+++ auth-service/src/main/java/com/turnquestlife/auth_service/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.turnquestlife.config;
+
+import org.sprigframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}

e2084987b149b7b040b4b7b462ff76e9b58b98f6
JohnMartin-19 <johnnkonge2020@gmail.com>
2026-07-05 08:59:12 +0300

    feat:updated the repository for some extended methods

--- client-service/src/main/java/com/turnquestlife/client_service/repository/ClientRepository.java
+++ client-service/src/main/java/com/turnquestlife/client_service/repository/ClientRepository.java
@@ -9,8 +9,24 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    String findByEmail(String email);
+    Optional<Client> findByEmail(String email);
+
     Optional<Client> findByIdNumber(String idNumber);
+
+    boolean existsByEmail(String email );
+
+    boolean existsByIdNumber(String idNumber);
+
+    Page<Client> findByKycStatus (KycStatus kycStatus, Pageable pagaeable);
+
+    @Query("SELECT c FROM Client c WHERE " +
+            "LOWER(c.firstName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(c.lastName)  LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(c.email)     LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "c.idNumber LIKE CONCAT('%', :keyword, '%')")
+    Page<Client> search(@Param("keyword") String keyword, Pageable pageable);
 }

bdadb113c5465fbb70e1297c4ec9719b685f9e81
JohnMartin-19 <johnnkonge2020@gmail.com>
2026-07-07 05:02:07 +0300

    feat:created the POST service

--- client-service/src/main/java/com/turnquestlife/client_service/service/ClientService.java
+++ client-service/src/main/java/com/turnquestlife/client_service/service/ClientService.java
@@ -15,3 +15,38 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@ReqiredArgsConstructor
+@Slf4j
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    //POST - create a client
+    @Transactional
+    public ClientResponse create(ClientRequest request) {
+        if(clientReposiotry.existsByEmail(reqiesy.getEmail())) {
+            throw new IllegalArguementException("Email already registered" + request.getEmail());
+        }
+        if (clientRepository.existsByIdNumber(request.getIdNumber())) {
+            throw new IllegalArguementException("ID Number already exists" + reqiest.getIdNumber());
+        }
+
+        Client client = Client.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(reques.getEmail())
+                .phone(request.getPhone())
+                .idNumber(request.getIdNumber())
+                .dateOfBirth(request.getDateOfBirth())
+                .gender(request.getGender())
+                .address(request.getAddress())
+                .occupation(request.getOccupation())
+                .build();
+
+        Client saved = clientRepository.save(client);
+        log.info("Client with ID: {}", saved.getId());
+        return toResponse(saved);
+    }
+}
